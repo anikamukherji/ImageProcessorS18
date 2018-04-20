fa752f0f379ca6b134221f6038f7fb3fd12a3b0a
VCM admin account <vcm@vcm-3551.vm.duke.edu>
2018-04-20 00:19:14 -0400

    models

---
+++ models.py
@@ -0,0 +1,18 @@
+from pymodm import fields, MongoModel
+
+class User(MongoModel):
+	email = fields.EmailField(primary_key=True) # because primary_key is True, we will need to query this field using the label _id
+	hist_times = fields.IntegerField() # the number of times to do histogram
+	contrast_times = fields.IntegerField()
+	log_times = fields.IntegerField()
+	reverse_times = fields.IntegerField()
+	upload_time = fields.ListField(field=fields.DateTimeField())
+	hist_time = fields.ListField(field=fields.DateTimeField())
+	contrast_time = fields.ListField(field=fields.DateTimeField())
+	log_times = fields.ListField(field=fields.DateTimeField())
+	reverse_times = fields.ListField(field=fields.DateTimeField())
+	image = fields.ListField(field=fields.ImageField())
+
+	class Meta:
+		write_concern = WriteConcern(j=True)
+        	connection_alias = 'image-app'
