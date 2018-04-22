79a14a3bcb4412180cbd8c7411750fc1ce58e82a
Zhiwei <zhiwei@Zhiweis-MacBook-Pro.local>
2018-04-22 11:52:53 -0400

    server1

---
+++ Server/64base.py
@@ -0,0 +1,57 @@
+import pymodm
+from pymodm import connect
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import database_functions
+import models
+import datetime
+app = Flask(__name__)
+CORS(app)
+connect("mongodb://vcm-3572.vm.duke.edu:27017/imaging_processing")  # open up connection to db  ????
+
+
+@app.route("/api/histogram_equalization", methods=["POST"])
+def store_heart_rate():
+    """
+        send back the histogram equalization processing result
+        """
+    r = request.get_json()  # parses the POST request body as JSON
+    start = datetime.datetime.now()
+    end = datetime.datetime.now()
+    process_time = start - end
+
+
+@app.route("/api/contrast-stretching", methods=["POST"])
+def store_heart_rate():
+    """
+        send back the contrast stretching processing result
+        """
+    r = request.get_json()  # parses the POST request body as JSON
+    start = datetime.datetime.now()
+    end = datetime.datetime.now()
+    process_time = start - end
+
+
+@app.route("/api/log_compression", methods=["POST"])
+def store_heart_rate():
+    """
+        send back the log compression processing result
+        """
+    r = request.get_json()  # parses the POST request body as JSON
+    start = datetime.datetime.now()
+    end = datetime.datetime.now()
+    process_time = start - end
+
+
+@app.route("/api/reverse_video", methods=["POST"])
+def store_heart_rate():
+    """
+        send back the reverse video processing result
+        """
+    r = request.get_json()  # parses the POST request body as JSON
+    start = datetime.datetime.now()
+    end = datetime.datetime.now()
+    process_time = start - end
+
+if __name__ == "__main__":
+    app.run()
